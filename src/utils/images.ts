7c7354fc0e214847ff40ce41355a82cc85ed4ece
ThatGuySam <sam@sam.lc>
2025-01-25 14:35:02 -0600

    fix: linting errors

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,54 +1,55 @@
-import type { ImageMetadata } from 'astro';
+import type { ImageMetadata } from 'astro'
 
-type ImageEntry = () => Promise<{ default: ImageMetadata }>;
-type ImageGlobResult = Record<string, ImageEntry>;
+type ImageEntry = () => Promise<{ default: ImageMetadata }>
+type ImageGlobResult = Record<string, ImageEntry>
 
 /** Load images from a directory */
 async function loadImages(): Promise<ImageGlobResult | undefined> {
-  let images: ImageGlobResult | undefined;
+  let images: ImageGlobResult | undefined
   try {
-    images = import.meta.glob<{ default: ImageMetadata }>('~/assets/images/**/*.{jpeg,jpg,png,gif}');
-  } catch (e) {
+    images = import.meta.glob<{ default: ImageMetadata }>('~/assets/images/**/*.{jpeg,jpg,png,gif}')
+  }
+  catch {
     // Do nothing if images cannot be loaded
   }
-  return images;
+  return images
 }
 
-let _images: ImageGlobResult | undefined;
+let _images: ImageGlobResult | undefined
 
 /** Get all images from ~/assets/images/ */
 export async function getImages() {
-  _images = _images || await loadImages();
-  return _images;
+  _images = _images || await loadImages()
+  return _images
 }
 
 /** Get image data from ~/assets/images/ */
 export async function getImage(imagePath: string) {
   if (!imagePath) {
-    return null;
+    return null
   }
 
   if (typeof _images === 'undefined') {
-    _images = await loadImages();
+    _images = await loadImages()
   }
 
   if (!_images || !_images[imagePath]) {
-    return null;
+    return null
   }
 
-  const image = await _images[imagePath]();
-  return image.default;
+  const image = await _images[imagePath]()
+  return image.default
 }
 
 /** Get image data from ~/assets/images/ */
 export async function findImage(imagePath: string) {
   if (!imagePath) {
-    return undefined;
+    return undefined
   }
 
   // For now only consume images using ~/assets alias (or absolute)
-  const images = await getImages();
-  const key = imagePath.replace('~/', '/src/');
+  const images = await getImages()
+  const key = imagePath.replace('~/', '/src/')
 
-  return images?.[key] ? (await images[key]()).default : undefined;
+  return images?.[key] ? (await images[key]()).default : undefined
 }
